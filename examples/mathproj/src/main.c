b8159e55cf784c194616e5a227ddff4edb29917f
Kevin Zimmerman <4733573+kczimm@users.noreply.github.com>
2025-02-20 22:01:11 -0600

    add example Cork project

---
+++ examples/mathproj/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../include/add.h"
+#include "../include/subtract.h"
+
+int main() {
+    int a = 10, b = 5;
+    printf("Add: %d + %d = %d\n", a, b, add(a, b));
+    printf("Subtract: %d - %d = %d\n", a, b, subtract(a, b));
+    return 0;
+}
---
+++ examples/mathproj/tests/test_main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "../include/headers.h"
+
+int main() {
+    printf("Running tests\n");
+    return 0;
+}
